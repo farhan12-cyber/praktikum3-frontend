7b1fcff35aa3429c90021624b3f042c83d7bd147
MSI <mftfn14@gmail.com>
2022-03-17 02:37:05 +0700

    menambahkan commit file

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import Hello from "./Hello";
+function Main() {
+    return(
+        <main>
+            <marquee><h4>Tetap Semangat Ngoding Walaupun Ga di semangatin ayang</h4></marquee>
+            <Hello name ="Muhammad" major ="Teknik informatika" />
+            <Hello name ="Farhan" major ="Teknik informatika" />
+            <Hello name ="Nadia" major ="Management " />
+            <Hello name ="Nur" major ="Akuntansi"/>
+            <Hello name = "Ivana" major ="Bahasa"/>
+        </main>
+    );
+}
+export default Main;
